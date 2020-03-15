7a475a208566d4526ea197371648c55d2215cbff
Iskren Ivanov <iskrenivanov@Iskrens-MacBook-Pro.local>
2020-03-15 17:16:16 +0200

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Account from './Home';
+
+function NavBar() {
+  return (
+    <div>
+		<nav>
+			<ul>
+				<li><Account/></li>
+				<li>Home</li>
+				<li>Home</li>
+				<li>Home</li>
+			</ul>
+		</nav>
+    </div>
+  );
+}
+
+export default NavBar;
