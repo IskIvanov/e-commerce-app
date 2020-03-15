71a60e10d787b781185bb7ffcb1a2aa10e15e192
Iskren Ivanov <iskrenivanov@Iskrens-MacBook-Pro.local>
2020-03-15 17:47:17 +0200

    Routing added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 import Account from './Account';
 import Home from './Home';
 import Cart from './Cart';
@@ -6,16 +12,42 @@ import Product from './Product';
 
 function NavBar() {
   return (
-    <div>
-		<nav>
-			<ul>
-				<li><Account/></li>
-				<li><Home/></li>
-				<li><Cart/></li>
-				<li><Product/></li>
-			</ul>
-		</nav>
-    </div>
+	<Router>
+		<div>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/account">Account</Link>
+					</li>
+					<li>
+						<Link to="/cart">Cart</Link>
+					</li>
+					<li>
+						<Link to="/product">Product</Link>
+					</li>
+				</ul>
+			</nav>
+
+		<Switch>
+			<Route path="/">
+				<Home />
+			</Route>
+			<Route path="/account">
+				<Account />
+			</Route>
+			<Route path="/cart">
+				<Cart />
+			</Route>
+			<Route path="/product">
+				<Product />
+			</Route>
+        </Switch>
+
+		</div>
+	</Router>
   );
 }
 
